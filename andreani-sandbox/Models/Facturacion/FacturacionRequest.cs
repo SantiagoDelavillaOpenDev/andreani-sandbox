399a549497bf873a970db0cc9c0608559ee6a842
SantiagoDelavillaOpenDev <delavilla.s@opendevpro.com>
2023-06-05 11:19:29 -0300

    add MaxLength Facturacion

--- andreani-sandbox/Models/Facturacion/FacturacionRequest.cs
+++ andreani-sandbox/Models/Facturacion/FacturacionRequest.cs
@@ -5,11 +5,17 @@ namespace andreani_sandbox.Models.Facturacion;
 public class FacturacionRequest
 {
     [Required]
+    [MaxLength(10)]
     public string Almacen { get; set; }
+    
     [Required]
+    [MaxLength(20)]
     public string ContratoDistribucion { get; set; }
+    
     [Required]
+    [MaxLength(10)]
     public string Instancia { get; set; }
+    
     [Required]
     public PedidoFacturacion Pedido { get; set; }
 }
--- andreani-sandbox/Models/Facturacion/PedidoFacturacion.cs
+++ andreani-sandbox/Models/Facturacion/PedidoFacturacion.cs
@@ -6,19 +6,42 @@ namespace andreani_sandbox.Models.Facturacion;
 
 public class PedidoFacturacion
 {
+    
+    [MaxLength(128)]
     public string ArchivoImpresionRemito { get; set; }
+    
+    [MaxLength(128)]
     public string Cot { get; set; }
+    
     public List<DatosAdicionalesFacturacion> DatosAdicionales { get; set; }
+    
+    [MaxLength(128)]
     public string FacturaLegal { get; set; }
+    
     public string FechaDeFacturacion { get; set; }
+    
     public string FechaEntrega { get; set; }
+   
+    [MaxLength(128)]
     public string LinkImpresionRemito { get; set; }
+    
     [Required]
+    [MaxLength(55)]
     public string NumeroOrdenExterna { get; set; }
+    
+    [MaxLength(128)]
     public string PrecioValorFc { get; set; }
+    
     [Required]
+    [MaxLength(55)]
     public string Propietario { get; set; }
+    
+    [MaxLength(55)]
     public string Remito { get; set; }
+    
+    [MaxLength(128)]
     public string TieneGestionCobranza { get; set; }
+    
+    [MaxLength(128)]
     public string ValorDeclarado { get; set; }
 }
