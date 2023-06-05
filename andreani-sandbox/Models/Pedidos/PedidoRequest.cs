b23e890479d5fccb9fbd75f37703f620a510441d
SantiagoDelavillaOpenDev <delavilla.s@opendevpro.com>
2023-06-05 11:54:40 -0300

    add MaxLength PedidoRequest

--- andreani-sandbox/Models/Pedidos/PedidoRequest.cs
+++ andreani-sandbox/Models/Pedidos/PedidoRequest.cs
@@ -5,15 +5,19 @@ namespace andreani_sandbox.Models.Pedidos;
 public class PedidoRequest
 {
     [Required]
+    [MaxLength(30)]
     public string almacen { get; set; }
     
     [Required]
+    [MaxLength(20)]
     public string contratoDistribucion { get; set; }
     
     [Required]
+    [MaxLength(20)]
     public string contratoWarehouse { get; set; }
     
     [Required]
+    [MaxLength(30)]
     public string instancia { get; set; }
     
     [Required]
